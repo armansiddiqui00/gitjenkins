8c083abff9ae4a2f198d3970aadb18141158d544
armandevops <aramnsidiqui0000@gmail.com>
2025-05-05 08:10:48 +0000

    some jenkins code pushed over git server

--- Servers.java
+++ Servers.java
@@ -1,5 +1,5 @@
-public class Servers {
-pulic static void main(String args[]) {
+public class servers {
+public static void main(String args[]) {
 System.out.println("Servers running");
 System.out.println("Some new server configuration");
 
